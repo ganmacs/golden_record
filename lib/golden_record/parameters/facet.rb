59de69a01a5a7b5f5cedf5bce70be3a0cd7becc6
ganmacs <ganmacs@gmail.com>
2017-07-19 18:07:21 +0900

    Add spec for facet

--- lib/golden_record/parameters/facet.rb
+++ lib/golden_record/parameters/facet.rb
@@ -4,12 +4,16 @@ module GoldenRecord
   module Parameters
     # http://lucene.apache.org/solr/guide/6_6/faceting.html
     class Facet < Base
-      def field_facet(field, opt)
-        @components << GoldenRecord::Parameter::FieldFacet.new(field, opt)
+      def add_field_facet(field, opt)
+        GoldenRecord::Parameter::FieldFacet.new(field, opt).tap do |v|
+          @components << v
+        end
       end
 
-      def range_facet(field, opt)
-        @components << GoldenRecord::Parameter::RangeFacet.new(field, opt)
+      def add_range_facet(field, range:, gap:, **opt)
+        GoldenRecord::Parameter::RangeFacet.new(field, opt.merge(range: range, gap: gap)).tap do |v|
+          @components << v
+        end
       end
     end
   end
---
+++ spec/golden_record/parameters/facet_spec.rb
@@ -0,0 +1,50 @@
+require "spec_helper"
+
+describe GoldenRecord::Parameters::Facet do
+  let(:facet_params) do
+    described_class.new
+  end
+
+  describe "#build" do
+    context "when calling add_field_facet" do
+      before do
+        facet_params.add_field_facet("id", {})
+      end
+
+      it "return a class instance of added value" do
+        param = facet_params.build
+        expect(param[:"facet.field"]).to eq("id")
+        expect(param[:facet]).to eq("true")
+      end
+    end
+
+    context "when calling add_range_facet" do
+      before do
+        facet_params.add_range_facet("id", range: 1..10, gap: 1)
+      end
+
+      it "return a class instance of added value" do
+        param = facet_params.build
+        expect(param[:facet]).to eq("true")
+        expect(param[:"facet.range"]).to eq("id")
+        expect(param[:"f.id.facet.range.start"]).to eq(1)
+        expect(param[:"f.id.facet.range.end"]).to eq(10)
+        expect(param[:"f.id.facet.range.gap"]).to eq(1)
+      end
+    end
+  end
+
+  describe "#add_field_facet" do
+    it "return a class instance of added value" do
+      v = facet_params.add_field_facet("id", {})
+      expect(v).to be_a(GoldenRecord::Parameter::FieldFacet)
+    end
+  end
+
+  describe "#add_range_facet" do
+    it "return a class instance of added value" do
+      v = facet_params.add_range_facet("id", range: 1..10, gap: 1)
+      expect(v).to be_a(GoldenRecord::Parameter::RangeFacet)
+    end
+  end
+end
