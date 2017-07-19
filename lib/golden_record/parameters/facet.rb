967f084a6b6884f5a2b1dac3e76866e1dd50e17c
ganmacs <ganmacs@gmail.com>
2017-07-19 23:06:39 +0900

    Override facet parameters explicitly

--- lib/golden_record/parameters/facet.rb
+++ lib/golden_record/parameters/facet.rb
@@ -6,12 +6,16 @@ module GoldenRecord
     class Facet < Base
       def add_field_facet(field, opt)
         GoldenRecord::Parameter::FieldFacet.new(field, opt).tap do |v|
+          # Override explicitly sice facet has parameter like f.<name>.<param_name>.
+          @components.reject! { |e| e.is_a?(GoldenRecord::Parameter::FieldFacet) }
           @components << v
         end
       end
 
       def add_range_facet(field, range:, gap:, **opt)
         GoldenRecord::Parameter::RangeFacet.new(field, range: range, gap: gap, **opt).tap do |v|
+          # Override explicitly sice facet has parameter like f.<name>.<param_name>.
+          @components.reject! { |e| e.is_a?(GoldenRecord::Parameter::RangeFacet) }
           @components << v
         end
       end
--- spec/golden_record/parameters/facet_spec.rb
+++ spec/golden_record/parameters/facet_spec.rb
@@ -16,6 +16,34 @@ describe GoldenRecord::Parameters::Facet do
         expect(param[:"facet.field"]).to eq("id")
         expect(param[:facet]).to eq("true")
       end
+
+      context "with option" do
+        it "override first value" do
+          facet_params.add_field_facet("id", sort: "count", mincount: 1, limit: 2, offset: 3)
+          param = facet_params.build
+
+          expect(param[:facet]).to eq("true")
+          expect(param[:"facet.field"]).to eq("id")
+          expect(param[:"f.id.facet.sort"]).to eq("count")
+          expect(param[:"f.id.facet.mincount"]).to eq(1)
+          expect(param[:"f.id.facet.limit"]).to eq(2)
+          expect(param[:"f.id.facet.offset"]).to eq(3)
+        end
+      end
+
+      context "when calling add_field_facet twice" do
+        it "override first value" do
+          facet_params.add_field_facet("id", sort: "count")
+          facet_params.add_field_facet("recipe", sort: "index")
+          param = facet_params.build
+
+          expect(param[:facet]).to eq("true")
+          expect(param[:"facet.field"]).to eq("recipe")
+          expect(param[:"f.recipe.facet.sort"]).to eq("index")
+
+          expect(param).not_to have_key("f.recipe.facet.sort")
+        end
+      end
     end
 
     context "when calling add_range_facet" do
@@ -30,6 +58,24 @@ describe GoldenRecord::Parameters::Facet do
         expect(param[:"f.id.facet.range.start"]).to eq(1)
         expect(param[:"f.id.facet.range.end"]).to eq(10)
         expect(param[:"f.id.facet.range.gap"]).to eq(1)
+      end
+
+      context "when calling add_range_facet twice" do
+        it "override first value" do
+          facet_params.add_range_facet("id", range: 1..10, gap: 1)
+          facet_params.add_range_facet("recipe", range: 2..11, gap: 2)
+          param = facet_params.build
+
+          expect(param[:facet]).to eq("true")
+          expect(param[:"facet.range"]).to eq("recipe")
+          expect(param[:"f.recipe.facet.range.start"]).to eq(2)
+          expect(param[:"f.recipe.facet.range.end"]).to eq(11)
+          expect(param[:"f.recipe.facet.range.gap"]).to eq(2)
+
+          expect(param).not_to have_key([:"f.id.facet.range.start"])
+          expect(param).not_to have_key([:"f.id.facet.range.end"])
+          expect(param).not_to have_key([:"f.id.facet.range.gap"])
+        end
       end
     end
   end
